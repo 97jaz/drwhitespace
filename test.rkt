0c393d9122f8c13b03b8152da22073697c7102a7
Jon Zeppieri <jon.zeppieri@meyouhealth.com>
2015-04-15 04:31:09 -0400

    Rewrite, with preferences

--- test.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket/base
-
-
-(define (foo n)
-
-  (if n
-      3
-      2   ))
--- tool.rkt
+++ tool.rkt
@@ -10,52 +10,88 @@
 
 (define strip-whitespace-mixin
   (mixin ((class->interface text%)) ()
+    (define/public (get-trailing-whitespace-ranges start end)
+      (define start-paragraph (send this position-paragraph start))
+      (define end-paragraph (send this position-paragraph end))
+
+      (define (para-whitespace p lower upper)
+        (define start (send this paragraph-start-position p))
+        (define end (send this paragraph-end-position p))
+        (define ws-start
+          (add1
+           (or (for*/first ([i (in-range (sub1 end) (sub1 start) -1)]
+                            [c (in-value (send this get-character i))]
+                            #:unless (char-whitespace? c))
+                 i)
+               (sub1 start))))
+
+        (define lo (max ws-start lower))
+        (define hi (min end upper))
+        (and (< lo hi)
+             (cons lo hi)))
+
+      (for*/list ([p (in-range start-paragraph (add1 end-paragraph))]
+                  [r (in-value (para-whitespace p start end))]
+                  #:when r)
+        r))
+
+    (define/public (delete-whitespace-ranges start end)
+      (for ([r (in-list (reverse (send this get-trailing-whitespace-ranges start end)))])
+        (send this delete (car r) (cdr r))))
+
+    (define/public (last-non-empty-paragraph)
+      (for*/first ([i (in-range (send this last-paragraph) -1 -1)]
+                   [s (in-value (send this paragraph-start-position i))]
+                   [e (in-value (send this paragraph-end-position i))]
+                   #:when (< s e))
+        i))
+
+    (define/public (delete-empty-trailing-lines)
+      (define last (send this last-paragraph))
+      (define last-nonempty (send this last-non-empty-paragraph))
+
+      (when (< last-nonempty last)
+        (send this
+              delete
+              (send this paragraph-start-position (add1 last-nonempty))
+              (send this paragraph-end-position last))))
+
     (define/augment (on-save-file filename format)
-      (unless (eq? format 'copy)
-        (strip-trailing-whitespace! this)
-        (remove-empty-trailing-paragraphs! this)))
+      (when (and (not (eq? format 'copy))
+                 (preferences:get PREF:ON-SAVE))
+        (define end (send this last-position))
+        (send this delete-whitespace-ranges 0 end)
+
+        (when (preferences:get PREF:DELETE-TRAILING-NEWLINES)
+          (send this delete-empty-trailing-lines))))
 
     (super-new)))
 
-(define (strip-trailing-whitespace! text-editor)
-  (for ([i (in-range (send text-editor last-paragraph) -1 -1)])
-    (define start (send text-editor paragraph-start-position i))
-    (define end (send text-editor paragraph-end-position i))
 
-    (define start-delete
-      (add1
-       (or
-        (for*/first ([j (in-range (sub1 end) (sub1 start) -1)]
-                     [c (in-value (send text-editor get-character j))]
-                     #:unless (char-whitespace? c))
-          j)
-        (sub1 start))))
+(define PREF:ON-SAVE 'drwhitespace:strip-on-save)
+(define PREF:DELETE-TRAILING-NEWLINES 'drwhitespace:delete-trailing-newlines)
 
-    (log-debug "delete in paragraph ~a [~s] from ~a - ~a" i (send text-editor get-text start end) start-delete end)
-    (log-debug "~s" (send text-editor get-character end))
+(preferences:set-default PREF:ON-SAVE #f boolean?)
+(preferences:set-default PREF:DELETE-TRAILING-NEWLINES #f boolean?)
 
-    (when (< start-delete end)
-      (send text-editor delete start-delete end))))
+(preferences:add-to-editor-checkbox-panel
+ (λ (panel)
+   (define vcontainer (new vertical-pane% [parent panel] [alignment '(left center)]))
 
-(define (last-non-empty-paragraph text-editor)
-  (for*/first ([i (in-range (send text-editor last-paragraph) -1 -1)]
-               [s (in-value (send text-editor paragraph-start-position i))]
-               [e (in-value (send text-editor paragraph-end-position i))]
-               #:when (< s e))
-    i))
+   (preferences:add-check
+    vcontainer
+    PREF:ON-SAVE
+    "Strip trailing whitespace before saving a file")
 
-(define (remove-empty-trailing-paragraphs! text-editor)
-  (define last (send text-editor last-paragraph))
-  (define last-nonempty (last-non-empty-paragraph text-editor))
+   (define hcontainer (new horizontal-pane% [parent vcontainer]))
+   (define spacer (new horizontal-pane% [parent hcontainer] [min-width 20] [stretchable-width #f]))
 
-  (when (< last-nonempty last)
-    (send text-editor
-          delete
-          (send text-editor paragraph-start-position (add1 last-nonempty))
-          (send text-editor paragraph-end-position last))))
+   (preferences:add-check
+    hcontainer
+    PREF:DELETE-TRAILING-NEWLINES
+    "Delete empty lines at the end of the file")))
 
+(drracket:get/extend:extend-definitions-text strip-whitespace-mixin)
 
 (define (phase1) (void))
 (define (phase2) (void))
-
-(drracket:get/extend:extend-definitions-text strip-whitespace-mixin)
